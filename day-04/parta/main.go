0a3557b8cc82b3699cda9f933814ecd70157f9be
HamzaMateen <hamzamateen1583@gmail.com>
2023-08-14 20:42:08 +0500

    add day-4 solutions in Go

---
+++ day-04/parta/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	currentDir, err := os.Getwd()
+
+	if err != nil {
+		panic(err)
+	}
+
+	inputFilePath := filepath.Join(currentDir, "./parta/input.txt")
+
+	// load the file
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// define a scanner
+	scanner := bufio.NewScanner(file)
+	totalOverlappingSections := 0
+
+	// scan the file
+	for scanner.Scan() {
+		line := scanner.Text()
+		// process the line here
+		// chop by ,
+		elfOne, elfTwo := func() (string, string) {
+			sections := strings.Split(line, ",")
+			return sections[0], sections[1]
+		}()
+
+		// extract ranges
+		rangeOneStart, rangeOneEnd := func() (int, int) {
+			_range := strings.Split(elfOne, "-")
+
+			start, _ := strconv.Atoi(_range[0])
+			end, _ := strconv.Atoi(_range[1])
+
+			return start, end
+		}()
+
+		rangeTwoStart, rangeTwoEnd := func() (int, int) {
+			_range := strings.Split(elfTwo, "-")
+
+			start, _ := strconv.Atoi(_range[0])
+			end, _ := strconv.Atoi(_range[1])
+
+			return start, end
+		}()
+
+		// compare
+
+		if rangeOneStart >= rangeTwoStart && rangeOneEnd <= rangeTwoEnd {
+			totalOverlappingSections += 1
+		} else if rangeTwoStart >= rangeOneStart && rangeTwoEnd <= rangeOneEnd {
+			totalOverlappingSections += 1
+		}
+	}
+
+	// print the number of overlapping ranges
+	fmt.Printf("The overlapping sections' count is: %d \n", totalOverlappingSections)
+}
